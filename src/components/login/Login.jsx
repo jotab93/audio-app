097f56f9a457ba96f42044d69fd5f61bc02f6173
Juan Bove <juansbove@gmail.com>
2023-07-05 14:33:45 -0300

    Agrego logica y formulario de login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,68 @@
-const Login = () => {
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+
+const Login = ({
+  showPassword,
+  handleShow,
+  handleSubmit,
+  handleChange,
+  errors,
+}) => {
   return (
-    <div>
-      <h1>Este es el login</h1>
+    <div style={{ paddingTop: "20px" }}>
+      <form onSubmit={handleSubmit}>
+        {/* Campo del email */}
+        <TextField
+          name="email"
+          label="Email"
+          variant="outlined"
+          onChange={handleChange}
+          error={errors.email ? true : false}
+          helperText={errors.email}
+        />
+
+        {/* Campo de la password */}
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            name="password"
+            onChange={handleChange}
+            error={errors.password ? true : false}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleShow}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            // label="Password"
+          />
+          <FormHelperText error={errors.password ? true : false}>
+            {errors.password}
+          </FormHelperText>
+        </FormControl>
+
+        <Button type="submit" variant="contained">
+          Ingresar
+        </Button>
+      </form>
     </div>
   );
 };
